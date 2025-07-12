6909775c849b595425df3d4ede4da06ba801b02a
Anton Anpilohov <162635210+tosentai@users.noreply.github.com>
2025-07-12 16:33:52 +0300

    ContactSection added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
 import { SkillsSection } from "@/components/SkillsSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
+import { ContactSection } from "@/components/ContactSection";
 
 export const Home = () => {
     return (
@@ -21,6 +22,7 @@ export const Home = () => {
                 <AboutSection />
                 <SkillsSection />
                 <ProjectsSection />
+                <ContactSection />
             </main>
             {/* Footer */}
         </div>
